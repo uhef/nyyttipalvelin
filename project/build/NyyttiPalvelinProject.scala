56c43dca13f16955b353b458b9b8bb3987d877c0
Timo Rantalaiho <Timo.Rantalaiho@reaktor.fi>
2011-03-03 18:28:58 +0200

    Try to get project sources

--- project/build/NyyttiPalvelinProject.scala
+++ project/build/NyyttiPalvelinProject.scala
@@ -2,15 +2,17 @@ import sbt._
 
 class NyyttiPalvelinProject(info: ProjectInfo) extends DefaultWebProject(info) with IdeaProject {
   val scalatraVersion = "2.0.0.M2"
-  val scalatra = "org.scalatra" %% "scalatra" % scalatraVersion
-  val servletApi = "org.mortbay.jetty" % "servlet-api" % "2.5-20081211" % "provided"
-  val liftJson = "net.liftweb" %% "lift-json" % "2.2"
+  val scalatra = "org.scalatra" %% "scalatra" % scalatraVersion withSources()
+  val servletApi = "org.mortbay.jetty" % "servlet-api" % "2.5-20081211" % "provided" withSources()
+  val liftJson = "net.liftweb" %% "lift-json" % "2.2" withSources()
 
   // Alternatively, you could use scalatra-specs
-  val scalatest = "org.scalatra" %% "scalatra-scalatest" % scalatraVersion % "test"
+  val scalatest = "org.scalatra" %% "scalatra-scalatest" % scalatraVersion % "test" withSources()
+
+  // val sc = "org.scalacheck" % "scalacheck" % "1.5" withSources()
 
   // Pick your favorite slf4j binding
-  val slf4jBinding = "ch.qos.logback" % "logback-classic" % "0.9.25" % "runtime"
+  val slf4jBinding = "ch.qos.logback" % "logback-classic" % "0.9.25" % "runtime" withSources()
 
   // http://groups.google.com/group/simple-build-tool/msg/1f17b43807d06cda
   override def testClasspath = super.testClasspath +++ buildCompilerJar
