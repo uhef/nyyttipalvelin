17a0992f089850200c2ae377c187b3005d619554
Timo Rantalaiho <Timo.Rantalaiho@reaktor.fi>
2011-03-03 17:19:33 +0200

    Initial stuff based on AlgoholismFilter of Aki Saarinen.

---
+++ src/main/scala/fi/akisaarinen/AlgoholismFilter.scala
@@ -0,0 +1,112 @@
+package fi.akisaarinen
+
+import org.scalatra._
+import java.net.URL
+import net.liftweb.json.JsonAST
+import net.liftweb.json.JsonDSL
+import net.liftweb.json.JsonDSL._
+import net.liftweb.json.JsonParser
+import net.liftweb.json.JsonParser._
+import scala.actors._
+import scala.actors.Actor._
+
+class AlgoholismFilter extends ScalatraFilter {
+
+  before {
+    ProcessingActor.start()
+  }
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Use POST</h1>
+      </body>
+    </html>
+  }
+  
+  post("/") {
+    val input = getBody(request.body, params.keys)
+    if (input.size > 0) {
+      val json = parse(input)
+      contentType = "application/json"
+      compact(process(json)) + "\n"
+    } else {
+      ""
+    }
+  }
+
+  notFound {
+    response.setStatus(404)
+    <html>
+      <body>
+        <h1>404</h1>
+      </body>
+    </html>
+  }
+
+  private def getBody(body: String, paramNames: Iterable[String]): String = {
+    // Sometimes scalatra seems to eat the body and put the content to first
+    // key of the map. I didn't want to invest time on finding out why, this'll do :-)
+    if (paramNames.size > 0) paramNames.head else body
+  }
+
+  private def process(json: JsonAST.JValue): text.Document = {
+    import net.liftweb.json.JsonAST._
+    import net.liftweb.json.JsonDSL._
+
+    parseRequestObjectFrom(json) match {
+        case Some(o) => { println(o); println(o.c); ProcessingActor ! o.c(1); }
+        case None => {}
+    }
+    json \\ "a" match {
+      case JField("a", JString("lol")) => render(List(1,3))
+      case _ => render(json)
+    }
+  }
+
+  private def parseRequestObjectFrom(json: JsonAST.JValue): Option[MyRequest] = {
+    implicit val formats = net.liftweb.json.DefaultFormats
+
+    try {
+      val o = json.extract[MyRequest]
+      return Some(o);
+    } catch {
+      case ex: net.liftweb.json.MappingException => return None
+    }
+  }
+}
+
+case class MyCField(id: Int, e: List[Int], f: Int)
+
+case class MyRequest(a: String, b: Int, c: List[MyCField], g: List[Int])
+
+object ProcessingActor extends Actor {
+  def act() {
+    loop {
+      react {
+        case (cField: MyCField) => println("Got C field with id " + cField.id)
+        case msg => println("Unhandled message: " + msg)
+      }
+    }
+  }
+}
+
+case class Enqueue(message: Any)
+
+case class Dequeue(actor: Actor)
+
+object ProcessingQueue extends Reactor[Any] {
+  import scala.collection.mutable.Queue
+
+  private val queue = new Queue[Any]
+
+  def act() {
+    loop {
+      react {
+        case Enqueue(d) => queue enqueue d
+        case Dequeue(a) if queue.nonEmpty => a ! (queue dequeue)
+        }
+    }
+  }
+}
+
