5d92b8469374ad13a514f9a6c4a144b3b97b1e4f
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2011-03-04 15:53:13 +0200

    Refactored Bruteforce algorithm, functions reuse

--- src/main/scala/fi/akisaarinen/BruteForceFillerAlgorithm.scala
+++ src/main/scala/fi/akisaarinen/BruteForceFillerAlgorithm.scala
@@ -14,22 +14,36 @@ class BruteForceFillerAlgorithm {
     knapsack
   }
 
+  def sortByContentWeightValue(i : Int): (ContentsItem, ContentsItem) => Boolean = {
+    (x, y) => {
+      x.value / x.weight(i) < y.value / y.weight(i)
+    }
+  }
+
   def sortToScarcestDimension(scarce : Scarcest, knapsack : List[ContentsItem]) = {
     scarce match {
-      case First => { knapsack.sortWith((x, y) => { x.value / x.weight(0) < y.value / y.weight(0) }) }
-      case Second => { knapsack.sortWith((x, y) => { x.value / x.weight(1) < y.value / y.weight(1) }) }
-      case Third => { knapsack.sortWith((x, y) => { x.value / x.weight(2) < y.value / y.weight(2) }) }
+      case First => { knapsack.sortWith(sortByContentWeightValue(0)) }
+      case Second => { knapsack.sortWith(sortByContentWeightValue(1)) }
+      case Third => { knapsack.sortWith(sortByContentWeightValue(2)) }
       case _ => println ("won't optimize")
     }
   }
 
+  def getCapacityContstraint(i :Int, capacity: Weight, knapsack: scala.List[ContentsItem]): Int = {
+    capacity.dimensions(i) - knapsack.map {
+      case x => {
+        x.contentsWeight.dimensions(i)
+      }
+    }.foldLeft(0)(_ + _)
+  }
+
   def calculateDimensionWhichIsScarcest(knapsack : List[ContentsItem], capacity : Weight) : Scarcest = {
     var first, second, third = Int.MaxValue
-    first = capacity.dimensions(0) - knapsack.map{ case x => { x.contentsWeight.dimensions(0) } }.foldLeft(0)(_ + _)
+    first = getCapacityContstraint(0, capacity, knapsack)
     if(capacity.dimensions.size > 1)
-      second = capacity.dimensions(1) - knapsack.map{ case x => { x.contentsWeight.dimensions(1) } }.foldLeft(0)(_ + _)
+      second = getCapacityContstraint(1, capacity, knapsack)
     if(capacity.dimensions.size > 2)
-      third = capacity.dimensions(2) - knapsack.map{ case x => { x.contentsWeight.dimensions(2) } }.foldLeft(0)(_ + _)
+      third = getCapacityContstraint(2, capacity, knapsack)
     if(first < second && first < third) return First
     if(second < first &&  second < third) return Second
     if(third < first && third < second) return Third
