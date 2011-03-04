68799fef409e31432f1cd7d74c2892035ddcacdd
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2011-03-04 14:21:55 +0200

    Added support for 1-3 length weights

--- src/main/scala/fi/akisaarinen/BruteForceFillerAlgorithm.scala
+++ src/main/scala/fi/akisaarinen/BruteForceFillerAlgorithm.scala
@@ -14,10 +14,20 @@ class BruteForceFillerAlgorithm {
     knapsack
   }
 
+  def sortToScarcestDimension(scarce : Scarcest, knapsack : List[ContentsItem]) = {
+    scarce match {
+      case First => println( "first"  )
+      case _ => println ("lol")
+    }
+  }
+
   def calculateDimensionWhichIsScarcest(knapsack : List[ContentsItem], capacity : Weight) : Scarcest = {
-    val first = capacity.dimensions(0) - knapsack.map{ case x => { x.contentsWeight.dimensions(0) } }.foldLeft(0)(_ + _)
-    val second = capacity.dimensions(1) - knapsack.map{ case x => { x.contentsWeight.dimensions(1) } }.foldLeft(0)(_ + _)
-    val third = capacity.dimensions(2) - knapsack.map{ case x => { x.contentsWeight.dimensions(2) } }.foldLeft(0)(_ + _)
+    var first, second, third = Int.MaxValue
+    first = capacity.dimensions(0) - knapsack.map{ case x => { x.contentsWeight.dimensions(0) } }.foldLeft(0)(_ + _)
+    if(capacity.dimensions.size > 1)
+      second = capacity.dimensions(1) - knapsack.map{ case x => { x.contentsWeight.dimensions(1) } }.foldLeft(0)(_ + _)
+    if(capacity.dimensions.size > 2)
+      third = capacity.dimensions(2) - knapsack.map{ case x => { x.contentsWeight.dimensions(2) } }.foldLeft(0)(_ + _)
     if(first < second && first < third) return First
     if(second < first &&  second < third) return Second
     if(third < first && third < second) return Third
--- src/test/scala/fi/akisaarinen/BruteForceFillerAlgorithmSuite.scala
+++ src/test/scala/fi/akisaarinen/BruteForceFillerAlgorithmSuite.scala
@@ -31,4 +31,9 @@ class BruteForceFillerAlgorithmSuite extends FunSuite with ShouldMatchers {
      val result = controller.calculateDimensionWhichIsScarcest(List(createItem(List(1,2,3), 3), createItem(List(2,3,4), 5)), Weight(List(3,43,7)))
      result should equal (Multiple)
   }
+
+   test("BruteForce filler should order based on the scarcest dimension") {
+     val result = controller.sortToScarcestDimension(First, List(createItem(List(1,2,3), 3), createItem(List(2,3,4), 5)))
+     val result2 = controller.sortToScarcestDimension(Second, List(createItem(List(1,2,3), 3), createItem(List(2,3,4), 5)))
+   }
 }
