f822436176d4a8e1cb88790d643d7b8663d2621d
Timo Rantalaiho <Timo.Rantalaiho@reaktor.fi>
2011-03-04 16:26:20 +0200

    Cleanup
    No functional changes

--- src/main/scala/fi/akisaarinen/WeightSumSorter.scala
+++ src/main/scala/fi/akisaarinen/WeightSumSorter.scala
@@ -1,13 +1,5 @@
 package fi.akisaarinen
 
-/**
- * Created by IntelliJ IDEA.
- * User: tuomjarv
- * Date: 3/4/11
- * Time: 10:16 AM
- * To change this template use File | Settings | File Templates.
- */
-
 class WeightSumSorter {
   private def importance(x: ContentsItem) = x.value / (x.weight.sum)
 
