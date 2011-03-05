10765737f51e0c1a27c89ea561ead7b63e2ca8b3
Timo Rantalaiho <Timo.Rantalaiho@reaktor.fi>
2011-03-05 05:55:20 +0200

    Run Tabu with all our different sorting algorithms.
    This seems to further improve a couple of results with round 6 data:
    
    Nyyttilog-1299295497294.txt:Total value: 52824
    Nyyttilog-1299295497294.txt:Total value: 94061
    Nyyttilog-1299295497294.txt:Total value: 13644
    Nyyttilog-1299295497294.txt:Total value: 59441
    Nyyttilog-1299295497294.txt:Total value: 89580
    Nyyttilog-1299295497294.txt:Total value: 62381
    
    Nyyttilog-1299296988279.txt:Total value: 52824
    Nyyttilog-1299296988279.txt:Total value: 94061
    Nyyttilog-1299296988279.txt:Total value: 13644
    Nyyttilog-1299296988279.txt:Total value: 60551
    Nyyttilog-1299296988279.txt:Total value: 89580
    Nyyttilog-1299296988279.txt:Total value: 63093

--- src/main/scala/fi/akisaarinen/ActorController.scala
+++ src/main/scala/fi/akisaarinen/ActorController.scala
@@ -50,8 +50,13 @@ class ActorController {
     val capacitySorter = new CapacityDimensionWeightedSorter
     val itemAverageWeightSorter = new ItemAverageWeightsSorter
     val brute = new BruteForceFillerAlgorithm
-    val tabu = new TabuAlgorithm(timeout - 10000, (new WeightSumSorter).internalPack(_, capacity))
-    val algorithms: List[Algorithm] = List(weightSorter, capacitySorter, itemAverageWeightSorter, brute, tabu)
+    val weightToTabu = new TabuAlgorithm(timeout - 10000, (new WeightSumSorter).internalPack(_, capacity))
+    val capaToTabu = new TabuAlgorithm(timeout - 10000, (new CapacityDimensionWeightedSorter).internalPack(_, capacity))
+    val itemAvgToTabu = new TabuAlgorithm(timeout - 10000, (new ItemAverageWeightsSorter).internalPack(_, capacity))
+
+    val algorithms: List[Algorithm] = List(weightSorter, capacitySorter, itemAverageWeightSorter, brute,
+      weightToTabu, capaToTabu, itemAvgToTabu)
+
     filterFittingItems(items, capacity) match {
       case Some(filteredItems) => {
         val resultsOfAlgorithms: Nyyttimap.ResultsOfAlgorithms = Nyyttimap.runAlgorithms(filteredItems, algorithms, capacity, timeout)
